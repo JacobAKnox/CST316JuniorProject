8241bad2d60c28c8040468fc3b7164c790bd2912
Jacob Knox <jacob.knox@oit.edu>
2023-12-01 17:47:30 -0800

    Added go button that logs if guess is correct or not

--- place-guesser/src/app/countryselect.js
+++ place-guesser/src/app/countryselect.js
@@ -3,28 +3,46 @@
 import { countries } from '@/data/countries';
 import React, {useState} from 'react'
 import CountryItem from './countryitem';
+import { make_guess } from '@/game/game';
 
 export default function CountrySearchBox() {
     const [searchInput, setSearchInput] = useState("");
+    const [country, setCountry] = useState({});
     const [tempHide, setTempHide] = useState(false);
+    const [validCountry, setValidCountry] = useState(false);
 
     const handleChange = (e) => {
-        e.preventDefault();
-        setSearchInput(e.target.value);
-        setTempHide(false);
+        e.preventDefault()
+        setSearchInput(e.target.value)
+        setTempHide(false)
+        setValidCountry(false)
     };
 
     const selectGuess = (guess) => {
         setSearchInput(guess.name)
+        setCountry(guess)
         setTempHide(true)
+        setValidCountry(true)
     };
+
+    const submitGuess = (e) => {
+        if (!validCountry) return;
+        make_guess(country)
+    }
  
     return (
         <div className="h-screen flex-1 items-center rounded-3xl justify-cent text-black">
-            <input className="w-[600px] px-5 py-3 rounded-t-3xl mb-0 outline-none transition"
-            value={searchInput}
-            onChange={handleChange}
-            placeholder="Input a country..."/>
+            <div className="w-[600px] px-5 py-3 flex bg-white rounded-t-3xl mb-0 outline-none transition">
+                <input className="grow focus:outline-none"
+                value={searchInput}
+                onChange={handleChange}
+                placeholder="Input a country..."/>
+                <button
+                onClick={submitGuess}
+                className="hover:bg-slate-200 p-0">
+                    Go
+                </button>
+            </div>
             {searchInput !== "" && !tempHide &&
                 <ul className="relative w-[600px] bg-white
                 rounded-b-3xl max-h-56 px-5 py-3 overflow-y-auto mt-0">
