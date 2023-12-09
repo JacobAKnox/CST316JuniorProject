c39e4a2ec2f0dde94123e05f7785cbd4f5c97772
Joseph Ten Eyck <eclipse6464@gmail.com>
2023-12-09 10:24:42 -0800

    Updated homepage, updated CSS, added gameplay page

---
+++ place-guesser/src/app/game/page.js
@@ -0,0 +1,5 @@
+export default function Game() {
+    return (
+        <h1>Gameplay Page</h1>
+    )
+}
--- place-guesser/src/app/page.js
+++ place-guesser/src/app/page.js
@@ -1,9 +1,29 @@
 import CountrySearchBox from './countryselect'
+import RootLayout from './layout'
+import GameLink from './gameLink'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CountrySearchBox/>
+      <html>
+        <head>
+          <title>Place Guesser</title>
+        </head>
+        <body>
+          <div class="topBar">
+            <div class="gameIcon-row">
+              <img src="icons/game-icon-100px.png" alt="FAIL" />
+            </div>
+          </div>
+          <div class="homePageTitleContainer">
+            <p class="homePageTitle">Welcome to</p>
+            <p class="homePageTitle">Place Guesser</p>
+            <div class="playButtonContainer">
+              <GameLink></GameLink>
+            </div>
+          </div>
+        </body>
+      </html>
     </main>
   )
 }
