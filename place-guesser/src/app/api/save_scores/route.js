9d467b48492d923b657fe9b1e50e965cb13bbe30
Federico Hernandez Jimenez <Freddy.hernandezjimenez@oit.edu>
2023-12-09 16:08:54 -0800

    Freddy:Scoring

--- place-guesser/src/app/api/save_scores/route.js
+++ place-guesser/src/app/api/save_scores/route.js
@@ -1,24 +1,24 @@
 // save_scores.js
 import clientPromise from '@/lib/database';
 
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        try {
-            const client = await clientPromise;
-            const db = client.db("PlaceGuesser");
+export default async function POST(req) {
+    try {
+        const client = await clientPromise;
+        const db = client.db("PlaceGuesser");
 
-            const { sessionID, guessCount } = req.body;
-            await db.collection("SaveGame").insertOne({ sessionID, guessCount });
+        const { sessionID, guessCount } = req.body;
+        await db.collection("SaveGame").insertOne({ sessionID, guessCount });
 
-            res.status(201).json({ message: 'Score saved' });
-        } catch (e) {
-            console.error(e);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    } else {
-        res.status(405).json({ error: 'Method Not Allowed' });
+        return new Response(JSON.stringify({ message: 'Score saved' }), {
+            status: 201,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    } catch (e) {
+        console.error(e);
+        Response.error(500).json({ error: 'Internal Server Error' });
     }
-}
-
+    }
 
 //Added a route.js and make sure to check the all the servers
